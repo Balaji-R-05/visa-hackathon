b9a1dcfd742d8d7fc7ad6c57f51d57417e342be4
dinesh2432 <ddk113311@gmail.com>
2026-01-05 01:07:05 +0530

    all work over....

--- server/routes/fileMetaDataExtraction.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { fileMetaDataExtraction } from "../controllers/file.controller.js";
-const router = express.Router();
-
-router.post("/", fileMetaDataExtraction)
-
-
-export default router;
